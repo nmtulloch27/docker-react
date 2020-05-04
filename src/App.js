be1e4374ab225f43d9c3f1b4ac55e5a6a1709208
Nathan Tulloch <nate@nathans-imac.home>
2020-05-04 15:56:12 -0400

    small change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Hi Nate M Tulloch
+            Hi Nate M Tulloch 2
         </p>
         <a
           className="App-link"
